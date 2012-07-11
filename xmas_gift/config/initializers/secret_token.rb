527745bec3abfb764cfa5f0d6b17789e3fcc037c
claudia <clauditamasias@gmail.com>
2012-07-10 22:01:46 -0500

    first commit

---
+++ xmas_gift/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XmasGift::Application.config.secret_token = '3cf670c03a6830f13fe1ba9c62be5105f6af25c86f26a19bc0c00502dd39ed3943598c78e0cf863a2790e3d9e12ad1a7166136c1ea32042d807d6cccb8620a7f'
